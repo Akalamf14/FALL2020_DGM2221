5a5cbfee0f57ca51f51079701f89844e20d4f29b
Unknown <akalamf14@gmail.com>
2020-09-30 21:48:48 -0600

    Game setup

---
+++ Walk and Run/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody rBody;
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float vertInput = Input.GetAxis("Vertical");
+        float horInput = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(vertInput, 0, horInput) * moveSpeed * Time.deltaTime;
+    }
+}
