d7d08a6952e71f59768aafa8462123f333437709
Unknown <akalamf14@gmail.com>
2020-10-06 22:59:35 -0600

    Jump and Double Jump Script

---
+++ Jump and Double Jump/Assets/Script/JumpBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpBehaviour : MonoBehaviour
+{
+    public float jumpHeight;
+    public bool isGrounded;
+    private Rigidbody gameCharacter;
+
+    void Start()
+    {
+        gameCharacter = GetComponent<Rigidbody>();
+    }
+
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            gameCharacter.AddForce(new Vector3(0,5,0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+
+}
