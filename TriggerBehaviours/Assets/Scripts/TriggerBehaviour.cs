92c57639369017c8cc9ce0b34edef2f15c022d88
Unknown <akalamf14@gmail.com>
2020-10-12 21:34:08 -0600

    Trigger Behaviours Script and scene

---
+++ TriggerBehaviours/Assets/Scripts/TriggerBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TriggerBehaviour : MonoBehaviour
+{
+    public Color defaultColor, newColor;
+    private MeshRenderer meshR;
+
+    private WaitForSeconds wfs;
+    public int holdTime = 3;
+    public GameObject door;
+
+    void Start()
+    {
+        meshR = GetComponent<MeshRenderer>();
+        meshR.material.color = defaultColor;
+
+        wfs = new WaitForSeconds(holdTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        newColor.a = 0.5f;
+        meshR.material.color = newColor;
+
+        door.SetActive(false);
+    }
+
+    private IEnumerator OnTriggerExit(Collider other)
+    {
+        yield return wfs;
+        meshR.material.color = defaultColor;
+        door.SetActive(true);
+    }
+    
+
+
+}
