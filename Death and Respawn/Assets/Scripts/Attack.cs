b35461fd3810de17c2c2643b5d0bd3dc850a40c1
Unknown <akalamf14@gmail.com>
2020-10-19 22:58:17 -0600

    Player Damage and Death half finished

---
+++ Death and Respawn/Assets/Scripts/Attack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public FloatData damage;
+    public Transform target;
+
+    public FloatData moveSpeed;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            transform.LookAt(target);
+            transform.Translate(Vector3.forward * moveSpeed.value * Time.deltaTime);
+        }
+    }
+
+    
+    void OnCollisionEnter(Collision other)
+    {
+        print("Enemy is Attacking!");
+        var health = other.gameObject.GetComponent<PlayerHealth>();
+
+        if (health != null)
+        {
+            health.TakeDamage(damage);
+        }
+    }
+}
---
+++ Death and Respawn/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+   /* THINGS NEEDED FOR PLAYER HEALTH
+
+   VARIABLES - CURRENT HEALTH, MAX HEALTH, SPAWNPOINT
+
+   FUNCTION - TRIGGER, COLLISION, TAKEDAMAGE
+
+   */
+
+   public FloatData currentHealth, maxHealth;
+   public Vector3Data spawnPoint;
+
+
+   private void Start()
+   {
+       currentHealth.value = maxHealth.value;
+       
+    }
+
+    public void TakeDamage(FloatData amount)
+    {
+        currentHealth.value -= amount.value;
+
+        if(currentHealth.value <= 0)
+        {
+            currentHealth.value = 0;
+            print("Game Over!");
+
+
+            currentHealth.value = maxHealth.value;
+        }
+    }
+
+}
