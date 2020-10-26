0b1806bdd05ffadb20ed5ed91123577bb17f8d33
Unknown <akalamf14@gmail.com>
2020-10-25 21:49:21 -0600

    Merge Sandbox 2

--- Merge/Assets/Scripts/IntData.cs
+++ Merge/Assets/Scripts/IntData.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 [CreateAssetMenu]
 public class IntData : ScriptableObject
 {
-    public int value;
+   public int value;
 }
---
+++ Merge/Assets/Scripts/TriggerEventsBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventsBehaviour : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent;
+    public float delayTime = 0.01f;
+    private WaitForSeconds waitObj;
+
+    private void start()
+    {
+        waitObj = new WaitForSeconds(delayTime);
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        yield return waitObj;
+        triggerEnterEvent.Invoke();
+    }
+    
+}
