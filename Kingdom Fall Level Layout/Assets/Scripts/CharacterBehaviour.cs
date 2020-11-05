337457d2c31cbac33f53b69bfcfca21ae01e28c4
Unknown <akalamf14@gmail.com>
2020-11-04 20:34:53 -0700

    Work on Player Movement using anims and state machines

--- Kingdom Fall Level Layout/Assets/Scripts/CharacterBehaviour.cs
+++ Kingdom Fall Level Layout/Assets/Scripts/CharacterBehaviour.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 [RequireComponent(typeof(CharacterController))]
 public class CharacterBehaviour : MonoBehaviour
 {
-    public float rotateSpeed = 120f, gravity = -9.81f, jumpForce = 10f;
+    public float rotateSpeed = 120f, gravity =-9.81f, jumpForce;
     public FloatData normalSpeed, fastSpeed;
-    public IntData playerJumpCount;
+    //public IntData playerJumpCount;
 
     protected CharacterController controller;
     protected Vector3 movement;
@@ -17,18 +16,19 @@ public class CharacterBehaviour : MonoBehaviour
     protected float vInput, hInput;
     protected FloatData moveSpeed;
 
-    private float yVar;
-    private int jumpCount;
-
-    public UnityEvent onDeathEvent;
-    public FloatData playerHealth;
+    protected float yVar;
+    //private int jumpCount;
 
     private void OnEnable()
     {
         moveSpeed = normalSpeed;
-        controller = GetComponent<CharacterController>();
+        controller.GetComponent<CharacterController>();
         StartCoroutine(Move());
+    }
 
+    private void OnDisable()
+    {
+        StopAllCoroutines();
     }
 
     protected IEnumerator Move()
@@ -36,8 +36,6 @@ public class CharacterBehaviour : MonoBehaviour
         canMove = true;
         while(canMove)
         {
-            OnHorizontal();
-            OnVertical();
             OnMove();
             yield return wffu;
         }
@@ -45,36 +43,24 @@ public class CharacterBehaviour : MonoBehaviour
 
     protected virtual void OnHorizontal()
     {
-        hInput = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed;
         transform.Rotate(0, hInput, 0);
     }
 
     protected virtual void OnVertical()
     {
-        vInput = Input.GetAxis("Vertical") * moveSpeed.value;
+        vInput = Input.GetAxis("Horizontal") * moveSpeed.value;
+        movement.Set(vInput, yVar, 0);
     }
 
     private void OnMove()
     {
-
-        if(playerHealth.value <= 0)
-        {
-            onDeathEvent.Invoke();
-        }
-
-        if(Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            moveSpeed = fastSpeed;
-        }
-
-        if(Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            moveSpeed = normalSpeed;
-        }
+        OnVertical();
+        OnHorizontal();
 
         yVar += gravity * Time.deltaTime;
 
-        if(controller.isGrounded && movement.y <0)
+        /*
+        if(controller.isGrounded && movement.y < 0)
         {
             yVar = -1f;
             jumpCount = 0;
@@ -85,14 +71,9 @@ public class CharacterBehaviour : MonoBehaviour
             yVar = jumpForce;
             jumpCount++;
         }
+        */
 
-        movement.Set(vInput,yVar,hInput);
         movement = transform.TransformDirection(movement);
         controller.Move((movement) * Time.deltaTime);
-    }
-    
-    private void OnDisable()
-    {
-        StopAllCoroutines();
     }
 }
