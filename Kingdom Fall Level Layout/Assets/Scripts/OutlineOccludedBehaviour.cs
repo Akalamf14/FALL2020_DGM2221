a6e7acd551d717a26b5cf583cbf8be136c75bf68
Unknown <akalamf14@gmail.com>
2020-11-10 01:34:23 -0700

    move with looking at mouse, outline character when behind objs

---
+++ Kingdom Fall Level Layout/Assets/Scripts/OutlineOccludedBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using cakeslice;
+using UnityEngine;
+
+public class OutlineOccludedBehaviour : MonoBehaviour
+{
+    public Outline playerOutline;
+
+    private void Start()
+    {
+        playerOutline.enabled = false;
+    }
+
+    private void Update()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, 10f, LayerMask.GetMask("Deafalt")))
+        {
+            playerOutline.enabled = true;
+        }
+        else
+        {
+            playerOutline.enabled = false;
+        }
+    }
+}
