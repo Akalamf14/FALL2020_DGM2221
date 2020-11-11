8fbde67a4b7914f608e42ab8985be109fd9ebbfb
Unknown <akalamf14@gmail.com>
2020-11-10 22:52:42 -0700

    attempt to create script that makes objs transparent

---
+++ Kingdom Fall Level Layout/Assets/Scripts/DistFromCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+public class DistFromCam : MonoBehaviour
+{
+    void Update()
+    {
+        GetInput();
+        ProcessInput();
+
+        distanceSquared = (transform.position - Camera.main.transform.position).sqrMagnitude;
+
+    }
+
+    public float Dist()
+    {
+        return distanceSquared;
+    }
+}
+*/
