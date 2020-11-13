c05375d4199d73e0d4ca864794da449e131e668b
Unknown <akalamf14@gmail.com>
2020-11-13 13:49:41 -0700

    Finished Reception Puzzle using Trigger Events

---
+++ Kingdom Fall Level Layout/Assets/Scripts/SelfDestructBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructBehaviour : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+    }
+
+}
