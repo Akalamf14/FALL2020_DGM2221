59988f5dadbc6d12b2a9cbb07bacf247c0c02ff8
Unknown <akalamf14@gmail.com>
2020-11-18 14:33:37 -0700

    Start Scene Set up

---
+++ Kingdom Fall Level Layout/Assets/Scripts/AIBehaviour/NavAgentBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavAgentBehaviour : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Transform destination;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        destination = transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        destination = other.transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        destination = transform;
+    }
+
+    private void Update()
+    {
+        agent.destination = destination.position;
+    }
+    
+}
--- Kingdom Fall Level Layout/Assets/Scripts/ScriptableObjects/GameAction.cs
+++ Kingdom Fall Level Layout/Assets/Scripts/ScriptableObjects/GameAction.cs
@@ -7,10 +7,16 @@ using UnityEngine.Events;
 public class GameAction : ScriptableObject
 {
     public UnityAction action;
+    public UnityAction<Transform> transformAction;
 
     public void Raise()
     {
         action?.Invoke();
+    }
+
+    public void Raise(Transform obj)
+    {
+        transformAction(obj);
     }
     
 }
