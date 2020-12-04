5d0b9dd8250335017417b108e9b6c1e8723cf33f
Unknown <akalamf14@gmail.com>
2020-12-03 20:47:14 -0700

    Work on RevealKey Script

---
+++ Kingdom Fall Level Layout/Assets/Scripts/KeyRevealBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class KeyRevealBehaviour : MonoBehaviour
+{
+    public List<int> objList;
+    public UnityEvent triggerEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEvent.Invoke();
+    }
+
+    public void KeyReveal()
+    {
+        objList.Remove(1);
+    }
+
+
+}
