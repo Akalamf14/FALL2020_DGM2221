d4251b76af58e6b3684b3ec9e7a484cc5940c32d
Unknown <akalamf14@gmail.com>
2020-12-12 18:19:04 -0700

    Reworking NPC Dialogue

---
+++ Kingdom Fall Level Layout/Assets/Scripts/DialogManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+
+    public Text dialogueText;
+    private Queue<string> sentences;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialog(Dialogue dialogue)
+    {
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    private void EndDialogue()
+    {
+        Debug.Log("End of Convo");
+    }
+}
--- Kingdom Fall Level Layout/Assets/Scripts/Dialogue.cs
+++ Kingdom Fall Level Layout/Assets/Scripts/Dialogue.cs
@@ -1,40 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
-public class Dialogue : MonoBehaviour
+[System.Serializable]
+public class Dialogue
 {
-    public TextMeshProUGUI textDisplay;
-    public string[] sentences;
-    private int index;
-    public float typingSpeed;
+    [TextArea(3, 10)]
+    public string [] sentences;
 
-    private void OnTriggerEnter()
-    {
-        StartCoroutine(Type());
-    }
-
-    IEnumerator Type()
-    {
-        foreach(char letter in sentences[index].ToCharArray())
-        {
-            textDisplay.text += letter;
-            yield return new WaitForSeconds(typingSpeed);
-        }
-    }
-
-    public void NextSentence()
-    {
-        if(index < sentences.Length - 1)
-        {
-            index++;
-            textDisplay.text = "";
-            StartCoroutine(Type());
-        }
-        else
-        {
-            textDisplay.text = "";
-        }
-    }
 }
