4bc00a32817093163d0549adfaa75841bd6b5b34
Unknown <akalamf14@gmail.com>
2020-12-15 23:02:29 -0700

    Writing all NPC Dialogue

--- Kingdom Fall Level Layout/Assets/Scripts/DialogManager.cs
+++ Kingdom Fall Level Layout/Assets/Scripts/DialogManager.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class DialogManager : MonoBehaviour
 {
-    public Text dialogueText;
+    public TextMeshProUGUI dialogueText;
     private Queue<string> sentences;
 
     public Animator animator;
